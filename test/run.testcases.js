738cbf645b41e27bd291c120199a140fdb94aba0
Dinesh Nagar <dineshnagarit@gmail.com>
2019-10-07 12:34:46 +0530

    Added required files

--- test/run.testcases.js
+++ test/run.testcases.js
@@ -1,1 +1,7 @@
-// To do
+require('./createparkslot.test.js');
+require('./park.test.js');
+require('./leavepark.test.js');
+require('./parkingstatus.test.js');
+require('./registration_numbers_for_cars_with_colour.test.js');
+require('./slot_numbers_for_cars_with_colour.test.js')
+require('./slot_number_for_registration_number.test.js');
